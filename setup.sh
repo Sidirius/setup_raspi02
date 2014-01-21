0dc40b67932ac0a9b0430e5068f85cfaecd9360e
Sidirius <sid@sh87.net>
2014-01-21 19:05:16 +0100

    boboptions

--- setup.sh
+++ setup.sh
@@ -12,4 +12,6 @@ wget https://github.com/brooc/boblight-rpi/raw/master/src/boblight-dispmanx
 chmod 755 boblight-dispmanx
 mv /home/xbian/.xbmc/userdata/advancedsettings.xml /home/xbian/.xbmc/userdata/advancedsettings.xml_$_now.bak
 cp advancedsettings.xml /home/xbian/.xbmc/userdata/advancedsettings.xml
+mv /home/xbian/boboptions.txt /home/xbian/boboptions.txt_$_now.bak
+cp boboptions.txt /home/xbian/boboptions.txt
 rm -rf /home/pi/setup_raspi02
